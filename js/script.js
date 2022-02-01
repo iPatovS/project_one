95a36e466f3c8f153d6092f6b3edaaa88fdbd9f3
Ipatov <tanseka1706@gmail.com>
2022-02-01 16:55:53 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// const result = confirm ('Баклан ты здесь?!');
+// console.log(result);
+
+// const answer = prompt ('Как звать тебя?', '');
+
+const answers = [];
+answers[0] = prompt('Как ваше Имя?', '');
+answers[1] = prompt('Как ваша Фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write (answers);
